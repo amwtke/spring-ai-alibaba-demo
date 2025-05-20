d17b94737226ac7782f53088f72aad399823620b
amwtke <amwtke@gmail.com>
2025-05-20 16:05:22 +0800

    视觉转文字

--- src/main/java/com/sca/ai/springaialibabademo/image/ImageController.java
+++ src/main/java/com/sca/ai/springaialibabademo/image/ImageController.java
@@ -1,22 +1,46 @@
 package com.sca.ai.springaialibabademo.image;
 
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
 import com.alibaba.cloud.ai.dashscope.image.DashScopeImageModel;
 import com.alibaba.cloud.ai.dashscope.image.DashScopeImageOptions;
+import com.alibaba.dashscope.aigc.multimodalconversation.MultiModalConversation;
+import com.alibaba.dashscope.aigc.multimodalconversation.MultiModalConversationParam;
+import com.alibaba.dashscope.aigc.multimodalconversation.MultiModalConversationResult;
+import com.alibaba.dashscope.common.MultiModalMessage;
+import com.alibaba.dashscope.common.Role;
+import com.alibaba.dashscope.exception.NoApiKeyException;
+import com.alibaba.dashscope.exception.UploadFileException;
+import com.alibaba.dashscope.utils.JsonUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.image.ImagePrompt;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 import static com.alibaba.cloud.ai.autoconfigure.dashscope.DashScopeImageProperties.DEFAULT_IMAGES_MODEL_NAME;
 
 @Slf4j
 @RestController
 public class ImageController {
-    private final DashScopeImageModel dashScopeImageModel;
+    static private final FileSystemResource audioFile = new FileSystemResource("src/main/resources/audio.mp3");
+    static private final FileSystemResource imageFile = new FileSystemResource("src/main/resources/multimodal.test.png");
 
-    ImageController(DashScopeImageModel dashScopeImageModel) {
+    @Value("${spring.ai.dashscope.api-key}")
+    private String apiKey;
+    private final DashScopeImageModel dashScopeImageModel;
+    private final ChatClient.Builder dashScopeChatClientBuilder;
+
+    ImageController(DashScopeImageModel dashScopeImageModel, ChatClient.Builder dashScopeChatClientBuilder) {
         this.dashScopeImageModel = dashScopeImageModel;
+        this.dashScopeChatClientBuilder = dashScopeChatClientBuilder;
     }
 
     @GetMapping(value = "/ai/image")
@@ -27,5 +51,38 @@ public class ImageController {
                         .withN(1)
                         .build()))
                 .getResult().getOutput().getUrl();
+    }
+
+    @GetMapping("/ai/cc/multi")
+    public String multimodality(@RequestParam(value = "msg", defaultValue = "在图片中你能看到什么？") String msg) {
+        return dashScopeChatClientBuilder.build()
+                .prompt(new Prompt(msg, DashScopeChatOptions.builder()
+//                        .withModel("qwen-vl-plus")
+                        .build()))
+                .user(u -> u.media(MimeTypeUtils.IMAGE_PNG, imageFile)
+//                                .media()
+                )
+                .call().content();
+    }
+
+    @GetMapping("/ai/cc/multi_sdk")
+    public String multimodality_sdk(@RequestParam(value = "msg", defaultValue = "在图片中你能看到什么？") String msg) throws NoApiKeyException, UploadFileException {
+        MultiModalConversation conv = new MultiModalConversation();
+        MultiModalMessage userMessage = MultiModalMessage.builder().role(Role.USER.getValue())
+                .content(Arrays.asList(
+//                        Collections.singletonMap("image", "https://dashscope.oss-cn-beijing.aliyuncs.com/images/dog_and_girl.jpeg"),
+//                        Collections.singletonMap("image", "https://dashscope.oss-cn-beijing.aliyuncs.com/images/tiger.png"),
+                        Collections.singletonMap("image", "https://dashscope.oss-cn-beijing.aliyuncs.com/images/rabbit.png"),
+                        Collections.singletonMap("image", "/Users/xiaojin/workspace/demo/spring-ai-alibaba-demo/src/main/resources/multimodal.test.png"),
+                        Collections.singletonMap("text", msg))).build();
+        MultiModalConversationParam param = MultiModalConversationParam.builder()
+                // 若没有配置环境变量，请用百炼API Key将下行替换为：.apiKey("sk-xxx")
+                .apiKey(apiKey)
+                // 此处以qwen-vl-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+                .model("qwen-vl-plus")
+                .message(userMessage)
+                .build();
+        MultiModalConversationResult result = conv.call(param);
+        return JsonUtils.toJson(result);
     }
 }
